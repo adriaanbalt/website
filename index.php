5696d99d27b143ef7ef37075d3caa9da3459eb67
adriaanbalt <adriaan@balt.us>
2013-11-26 17:53:41 -0500

    fixing the merge by removing extra stuff

--- index.php
+++ index.php
@@ -36,38 +36,6 @@
 			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrxade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 		<![endif]-->
 
-		<div id="navigation">
-			<a href="javascript:void(0);" class="menu-btn menu-light"></a>
-			<a href="javascript:void(0);" class="menu-btn menu-dark"></a>
-			<a href="javascript:void(0);" class="menu-btn close-btn hidden"></a>
-			<nav>
-				<a href="#introduction" class="nav-btn" data-destination="introduction">
-					<span class="align"></span>
-					<span class="copy">INTRODUCTION</span>
-				</a>
-				<a href="#work" class="nav-btn" data-destination="work">
-					<span class="align"></span>
-					<span class="copy">WORK</span>
-				</a>
-				<a href="#recognition" class="nav-btn" data-destination="recognition">
-					<span class="align"></span>
-					<span class="copy">RECOGNITION</span>
-				</a>
-			</nav>
-		</div>
-
-		<div id="content">
-			
-			<section id="intro">
-				<span class="align"></span>
-				<div class='copy'>
-					<div class="logo"></div>
-					<div class="divide"></div>
-					<div class="year">2013</div>
-				</div>
-			</section>
-
-=======
 		<div id="content">
 			
 			<div id="intro">
@@ -79,7 +47,7 @@
 
 			<header class="waypointDest dark">
 				<span class="align"></span>
-				<div class="copy">
+				<div class="vcenter">
 					<div class="logo"></div>
 				</div>
 			</header>
@@ -104,7 +72,7 @@
 										<div class="cover">
 											<div class='slogan'>
 												<span class="align"></span>
-												<div class="copy">
+												<div class="vcenter">
 													<div class="play-btn"></div>
 													<h4><%= panel.video.headline %></h4>
 													<h2><%= panel.video.copy %></h2>
@@ -159,7 +127,6 @@
 
 			</footer>
 
->>>>>>> 826394fd3339321687e35304d52e977e14d13c31
 		</div> <!-- #content -->
 
 		<?php if ($env == 'PROD') { ?>
@@ -183,10 +150,6 @@
 		<script src="js/BALT.intro.js"></script>
 		<script src="js/BALT.nav.js"></script>	
 		<script src="js/BALT.reel.js"></script>	
-<<<<<<< HEAD
-		<script src="js/BALT.spritAnimation.js"></script>
-=======
->>>>>>> 826394fd3339321687e35304d52e977e14d13c31
 		<script src="js/BALT.templates.js"></script>
 		<script src="js/BALT.videosVimeo.js"></script>
 		<?php } ?>
@@ -194,297 +157,6 @@
 		<script>videojs.options.flash.swf = "video-js.swf";</script>
 
 		<script type="text/template" id="video-template">
-<<<<<<< HEAD
-			<iframe id="<%= data.id %>" src="http://player.vimeo.com/video/<%= data.src %>?api=1&player_id=<%= data.id %>" width="100%" height="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-		</script>
-		
-		<script type="text/template" id="introduction-template">
-			<section data-id="<%= data.name.toLowerCase() %>" class="<%= data.name.toLowerCase() %> chapter <%= data.status %>">
-				<div class="container">
-					<% if ( data.video ){ %>
-						<div class='videos-layer waypointDest dark'>
-							<div class="video-viewport ratio16_9">
-								<div class="inside">
-									<div class="layer slogan">
-										<span class="align"></span>
-										<div class="copy">
-											<% _.each( data.slogan, function(elem,key,list){ %>
-												<%= elem %>
-											<%});%>
-										</div>
-									</div>
-									<% if ( data.video.media ){ %>
-										<div class="video layer">
-											<% if ( data.video.media.mp4 ){ %>
-												<div class="video-player fullbrowser main-vid" id="video-main-<%= data.video.id %>" data-src="<%= data.video.media.mp4 %>" data-loop="false" data-muted="false" data-autoplay="false"></div>
-											<% } %>
-										</div>
-									<% } %>
-									<% if ( data.video.teaser ){ %>
-										<div class="cover">
-										<% if ( 'ontouchstart' in window ){ %>
-											<img class="still" data-src="<%= data.video.teaser.still %>"/>
-										<% } else if ( data.video.teaser.mp4 ){ %>
-											<div class="video-player teaser" id="video-teaser" data-src="<%= data.video.teaser.mp4 %>" data-loop="true" data-muted="true" data-autoplay="true"></div>
-										<% } else if ( data.video.teaser.gif){ %>
-											<div class="gif sprite-animation" style="background-image: url(<%= data.video.teaser.gif %>);" data-frames-len="<%= data.video.teaser.framesLength %>" data-aspect-ratio="<%= data.video.teaser.aspectRatio %>"></div>
-										<% } %>
-										</div>
-									<% } %>
-								</div>
-							</div>
-						</div>
-					<% } %>
-
-					<a class="arrow-pulse waypointDest dark" href="#beginning"></a>
-					<h3 class="inner waypointDest light beginning"><%= data.header %></h3>
-					<div class="inner paragraph waypointDest light">
-						<% _.each( data.paragraph, function(p,key,list){ %>
-							<p><%= p %></p>
-						<%});%>
-						<div class="names">
-							<% _.each( data.names, function(name,key,list){ %>
-								<div><%= name %></div>
-							<%});%>
-						</div>
-					</div>
-				</div>
-
-				<% if ( data.nextSection ){ %>
-					<a class="next-btn waypointDest dark" href="#<%= data.nextSection.toLowerCase() %>" data-destination="<%= data.nextSection.toLowerCase() %>">
-						<h6><span>NEXT SECTION:</span><%= data.nextSection %></h6>
-					</a>
-				<% } %>
-			</section>
-		</script>
-
-		<script type="text/template" id="copy-template">
-			<section data-id="<%= data.name.toLowerCase() %>" class="<%= data.name.toLowerCase() %> chapter <%= data.status %>">
-				
-				<div class="container">
-
-					<header class='waypointDest dark'>
-						
-						<% if ( data.header.video ){ %>
-							<div class='videos-layer'>
-								<div class="video-viewport ratio16_9">
-									<div class="inside">
-										<div class="layer slogan">
-											<span class="align"></span>
-											<div class="copy"> <%= data.header.copy %> </div>
-										</div>
-										<% if ( data.header.video.media ){ %>
-											<div class="video layer">
-												<% if ( data.header.video.media.mp4 ){ %>
-													<div class="video-player fullbrowser main-vid" id="video-main-<%= data.header.video.id %>" data-src="<%= data.header.video.media.mp4 %>" data-loop="false" data-muted="false"></div>
-												<% } %>
-											</div>
-										<% } %>
-										<div class="cover">
-											<% if ( 'ontouchstart' in window ){ %>
-												<img class="still" data-src="<%= data.header.video.teaser.still %>"/>
-											<% } else if ( data.header.video.teaser.mp4){ %>
-												<div class="video-player teaser" id="video-teaser-<%= data.header.video.id %>" data-src="<%= data.header.video.teaser.mp4 %>" data-loop="true" data-muted="true"></div>
-											<% } else if ( data.header.video.teaser.gif){ %>
-												<div class='text sprite-animation' style="background-image: url(<%= data.header.video.teaser.gif %>);" data-frames-len="<%= data.header.video.teaser.framesLength %>" data-aspect-ratio="<%= data.header.video.teaser.aspectRatio %>"></div>
-											<% } else if ( data.header.video.teaser.still ) { %>
-												<div class="still bgimg" data-src="<%= data.header.video.teaser.still %>"></div>
-											<% } %>
-										</div> 
-									</div>
-								</div>
-							</div>
-						<% } else if ( 'ontouchstart' in window ){ %>
-							<div class='text-mobile' style="background-image:url('<%= data.header.headerBG.still %>');">
-								<span class="align"></span>
-								<div class="copy"><%= data.header.copy %></div>
-							</div>
-						<% } else if ( data.header.headerBG.gif ) { %>
-							<div class="ratio16_9">
-								<div class="inside">
-									<div class='text sprite-animation' style="background-image: url(<%= data.header.headerBG.gif %>);" data-frames-len="<%= data.header.headerBG.framesLength %>" data-aspect-ratio="<%= data.header.headerBG.aspectRatio %>">
-										<span class="align"></span>
-										<div class="copy"><%= data.header.copy %></div>
-									</div>
-								</div>
-							</div>
-						<% } else if ( data.header.headerBG.still ){ %>
-							<div class='text' style="background-image:url('<%= data.header.headerBG.still %>');">
-								<span class="align"></span>
-								<div class="copy"><%= data.header.copy %></div>
-							</div>
-						<% } else { %>
-							<div class='text'>
-								<span class="align"></span>
-								<div class="copy"><%= data.header.copy %></div>
-							</div>
-						<% } %>
-
-					</header>
-
-					<% var iterator = 0, additionalClass = '';
-						_.each(data.blocks, function(block,key,list){
-						iterator++;
-						if ( iterator == 1 ) {
-							additionalClass = ' first';
-						} else if ( iterator == data.blocks.length ) {
-							additionalClass = ' last';
-						} else {
-							additionalClass = '';
-						}
-						%>
-						<% if ( block.stats ) { %>
-							<div class="stats<%= additionalClass %> waypointDest dark">
-								<ul class='wrapper'>
-									<% if ( block.logo ) { %>
-										<img class='stats-logo' data-src="<%= block.logo %>"/>
-									<% } %>
-									<% _.each( block.stats, function(stat,key,list){ %>
-									<li>
-										<span class="align"></span>
-										<span class="copy">
-											<% if ( stat.stars ) { %>
-												<img class="stars" data-src="<%= stat.stars %>"/>
-											<% } %>
-											<% if ( stat.number ) { %>
-												<dt><%= stat.number %></dt>
-											<% } %>
-											<% if ( stat.copy ) { %>
-												<dd><%= stat.copy %></dd>
-											<% } %>
-										</span>
-									</li>
-									<%});%>
-								</ul>
-							</div>
-						<% } else if ( block.clients ) { %>
-							<div class="relationships wrapper<%= additionalClass %> waypointDest light">
-								<h3 class='inner'><%= block.header %></h3>
-								<ul class="relations">
-									<% _.each(block.clients, function(client,key,list){ %>
-										<li>
-											<span class='align'></span>
-											<img data-src="<%= client.logo %>"/>
-										</li>
-									<%});%>
-								</ul>
-							</div>
-						<% } else if ( block.awards ) { %>
-							<div class="awards wrapper<%= additionalClass %>  waypointDest light">
-								<h3 class='inner'><%= block.header %></h3>
-								<ul>
-									<% _.each(block.awards, function(award,key,list){ %>
-										<li>
-											<object data="<%= award.image %>" type="image/svg+xml"></object>
-										</li>
-									<%});%>
-								</ul>
-							</div>
-						<% } else if ( block.press ) { %>
-							<div class="press wrapper-small<%= additionalClass %> waypointDest light">
-								<h3 class='inner'><%= block.header %></h3>
-								<ul>
-									<% _.each(block.press, function(item,key,list){ %>
-										<li>
-											<a href="<%= item.link %>" target="_blank"><h2><%= item.article %></h2></a>
-											<h1><%= item.source %></h1>
-											<img data-src="<%= item.image %>"/>
-										</li>
-									<%});%>
-								</ul>
-							</div>
-						<% } else if ( block.charts) { %>
-							<div class="charts<%= additionalClass %> waypointDest light">
-								<div class="inner">
-									<h3><%= block.header %></h3>
-								</div>
-								<ul class='wrapper'>
-								<% _.each(block.charts, function(chart,key,list){ %>
-									<li>
-										<canvas data-percent="<%= chart.percentage %>" id="<%= chart.uniqueID %>" width="160" height="160">
-											<p><%= chart.percentage %>&#37;</p>
-										</canvas>
-										<h2><%= chart.copy %></h2>
-									</li>
-								<%});%>
-								</ul>
-							</div>
-						<% } else if ( block.quote ) { %>
-							<div class="paragraph<%= additionalClass %> waypointDest light">
-								<div class="wrapper-small">
-									<h3><%= block.header %></h3>
-									<h2><%= block.quote %></h2>
-								</div>
-							</div>
-						<% } else if ( block.image ) { %>
-							<div class="paragraph<%= additionalClass %> <%= block.special_class %>">
-
-								<% if ( block.image_size == 'fullbleed' ) {
-									sizeClass = 'wrapper-large';
-								} else {
-									sizeClass = 'wrapper-small';
-								} %>
-								<div class="<%= additionalClass %>">
-									<% if ( block.image ) { %>
-										<img data-src="<%= block.image %>"/>
-									<% } %>
-								</div>
-							</div>
-						<% } else { %>
-							<div class="paragraph<%= additionalClass %> waypointDest light">
-								<div class="wrapper-small">
-									<div class='inner'>
-										<% if (block.header) { %>
-											<h3><%= block.header %></h3>
-										<% } %>
-										<% _.each( block.paragraph, function(p,key,list){ %>
-											<p><%= p %></p>
-										<%});%>
-									</div>
-								</div>
-							</div>
-						<% } %>
-					<%});%>
-
-					<% if ( data.thankyou ){ %>
-						<header class='waypointDest thankyou dark'>
-							
-							<% if ( 'ontouchstart' in window ){ %>
-								<div class='text-mobile'>
-									<span class="align"></span>
-									<div class="copy"><h1>THANK YOU</h1><h2>We appreciate your consideration</h2></br><%= data.thankyou.copy %></div>
-								</div>
-							<% } else if ( data.thankyou.gif ) { %>
-								<div class="ratio16_9">
-									<div class="inside">
-										<div class='text sprite-animation' style="background-image: url(<%= data.thankyou.gif %>);" data-frames-len="<%= data.thankyou.framesLength %>" data-aspect-ratio="<%= data.thankyou.aspectRatio %>">
-											<span class="align"></span>
-											<div class="copy"><%= data.thankyou.copy %></div>
-										</div>
-									</div>
-								</div>
-							<% } else if ( data.thankyou.still ){ %>
-								<div class='text' style="background-image:url('<%= data.thankyou.still %>');">
-									<span class="align"></span>
-									<div class="copy"><%= data.thankyou.copy %></div>
-								</div>
-							<% } else { %>
-								<div class='text' style="background-image:url('<%= data.thankyou.still %>');">
-									<span class="align"></span>
-									<div class="copy"><%= data.thankyou.copy %></div>
-								</div>
-							<% } %>
-
-						</header>
-					<% } %>
-				</div>
-
-				<% if ( data.nextSection ){ %>
-					<a class="next-btn waypointDest dark" href="#<%= data.nextSection.toLowerCase() %>" data-destination="<%= data.nextSection.toLowerCase() %>">
-						<h6><span>NEXT SECTION:</span><%= data.nextSection %></h6>
-					</a>
-				<% } %>
-=======
 			<div class='videoplayer'>
 				<iframe id="<%= data.id %>" src="http://player.vimeo.com/video/<%= data.src %>?api=1&player_id=<%= data.id %>" width="100%" height="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 			</div>
@@ -519,58 +191,12 @@
 						<%});%>
 					</div>
 				</div>
->>>>>>> 826394fd3339321687e35304d52e977e14d13c31
 			</section>
 		</script>
 
 		<script type="text/template" id="accordion-template">
 			<section data-id="<%= data.name.toLowerCase() %>" class="<%= data.name.toLowerCase() %> chapter <%= data.status %> waypointDest dark">
 
-<<<<<<< HEAD
-				<div class="container waypointDest dark">
-
-					<header class='waypointDest dark'>
-
-						<% if ( 'ontouchstart' in window ){ %>
-							<div class='text-mobile' style="background-image:url('<%= data.header.headerBG.still %>');">
-								<span class="align"></span>
-								<div class="copy"><%= data.header.copy %></div>
-							</div>
-						<% } else if ( data.header.headerBG.gif ) { %>
-							<div class="ratio16_9">
-								<div class="inside">
-									<div class='text sprite-animation' style="background-image: url(<%= data.header.headerBG.gif %>);" data-frames-len="<%= data.header.headerBG.framesLength %>" data-aspect-ratio="<%= data.header.headerBG.aspectRatio %>">
-										<span class="align"></span>
-										<div class="copy"><%= data.header.copy %></div>
-									</div>
-								</div>
-							</div>
-						<% } else if ( data.header.headerBG.still ){ %>
-							<div class='text' style="background-image:url('<%= data.header.headerBG.still %>');">
-								<span class="align"></span>
-								<div class="copy"><%= data.header.copy %></div>
-							</div>
-						<% } else { %>
-							<div class='text'>
-								<span class="align"></span>
-								<div class="copy"><%= data.header.copy %></div>
-							</div>
-						<% } %>
-
-					</header>
-					
-					<div class="accordion waypointDest dark">
-
-						<% _.each(data.studies, function(study,key,list){ %>
-
-							<article id="<%= study.id %>" class="waypointDest dark">
-								<a class="button" href="javascript:void(0);">
-									<div style="background-image: url( '<%= study.navBG %>' );"></div>
-									<h4><%= study.navTitle %></h4>
-								</a>
-								<div class="content">
-									<div class="me clearfix">
-=======
 					<div class="accordion waypointDest dark">
 
 						<% var iterator = 0, indexClass = '';
@@ -591,7 +217,6 @@
 								</a>
 								<div class="content">
 									<div class="panels clearfix">
->>>>>>> 826394fd3339321687e35304d52e977e14d13c31
 
 									<% _.each( study.panels, function(panel,key,list){
 										if ( panel.video ){ %>
@@ -616,14 +241,6 @@
 													<% } %>
 												</a>
 											</div>
-<<<<<<< HEAD
-										<% } else if ( panel.quote ) { %>
-											<div class="quote clearfix">
-												<h2><%= panel.quote %></h2>
-												<p><%= panel.source %></p>
-											</div>
-=======
->>>>>>> 826394fd3339321687e35304d52e977e14d13c31
 										<% } else if ( panel.stats ) { %>
 											<div class="stats">
 												<ul class='wrapper'>
@@ -673,19 +290,6 @@
 						<%});%>
 
 					</div>
-<<<<<<< HEAD
-					</div>
-				</div>
-
-				<% if ( data.nextSection ){ %>
-					<a class="next-btn waypointDest dark" href="#<%= data.nextSection.toLowerCase() %>" data-destination="<%= data.nextSection.toLowerCase() %>">
-						<h6><span>NEXT SECTION:</span><%= data.nextSection %></h6>
-					</a>
-				<% } %>
-			</section>
-		</script>
-
-=======
 
 				</div>
 
@@ -726,8 +330,6 @@
 			</footer>
 		</script>
 
-
->>>>>>> 826394fd3339321687e35304d52e977e14d13c31
 		<script>
 			var _gaq = _gaq || [];
 			_gaq.push(['_setAccount', 'UA-###-1']);
